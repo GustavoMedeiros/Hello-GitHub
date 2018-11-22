bdb6dafe41a69e0b8459ac93318b5cd8802fae0a
Gustavo Medeiros <gustavommoyses@gmail.com>
2018-11-22 19:15:44 -0200

    Adicionei arquivos

---
+++ Hello Git/src/git/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package git;
+
+/**
+ * @author Gustavo
+ * Primeiro projeto feito no github para aprendizado
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
